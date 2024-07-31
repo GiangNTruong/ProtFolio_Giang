4d95f27f57a3736b790bf336494f12322861de0c
Tunnnnnz <tuan250100@gmail.com>
2024-08-01 02:50:06 +0700

    portfolio new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -75,7 +75,7 @@ function Home() {
               </div>
               <div className=" space-y-2">
                 <a
-                  href="https://www.example.com"
+                  href="https://www.topcv.vn/xem-cv/B1BTBgUCUlVTUFcEV1QDBFkGVFYGCgUBVgQEVwb205"
                   class="inline-block px-6 py-3 bg-blue-500 text-white font-semibold text-center rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
                 >
                   More about me
